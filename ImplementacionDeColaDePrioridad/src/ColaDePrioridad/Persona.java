6819035af222d6efd53e0c37d5eb669822a19e2b
NahuelRepetto <89661407+NahuelRepetto@users.noreply.github.com>
2022-05-16 18:53:01 -0300

    Update Persona.java

--- ImplementacionDeColaDePrioridad/src/ColaDePrioridad/Persona.java
+++ ImplementacionDeColaDePrioridad/src/ColaDePrioridad/Persona.java
@@ -9,15 +9,7 @@ public class Persona implements Comparable<Persona>{
 		this.nombre = nombre;
 		this.prioridad = prioridad;
 	}
-
-	public String getNombre() {
-		return nombre;
-	}
 	
-	public int getPrioridad() {
-		return prioridad;
-	}
-
 	@Override
 	public String toString() {
 		return nombre + " prioridad= " + prioridad;
